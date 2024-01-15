776d821d68ed2961755d4ebdd2f09bd74fd3b898
Benjamin Boule <benjamin.boule@tehtek.com>
2024-01-15 10:49:06 +0100

    3rd commit

--- src/teh_admin/settings.py
+++ src/teh_admin/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR_2 = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -121,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR_2, 'site', 'public', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR_2, 'site', 'public', 'media')	
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
